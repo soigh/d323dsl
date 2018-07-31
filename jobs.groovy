aeb2a006ca6bae19c875c3fc0ec9e9055afbfa02
Glebk0 <glebko123@gmail.com>
2018-07-31 15:58:50 +0300

    finished childs

--- jobs.groovy
+++ jobs.groovy
@@ -1,4 +1,3 @@
-
 job("MNTLAB-hviniarski-main-build-job") {
     description()
     keepDependencies(false)
@@ -37,8 +36,34 @@ job("MNTLAB-hviniarski-main-build-job") {
     disabled(false)
     concurrentBuild(false)
 }
+def git_info = ("git ls-remote -h https://github.com/MNT-Lab/d323dsl").execute()
+def branches = git_info.text.readLines().collect { it.split()[1].replaceAll('refs/heads/', '')}.unique()
+
 for(i in 1..4) {
     job("MNTLAB-hviniarski-child${i}-build-job") {
-
+        parameters {
+            choiceParam('BRANCH_NAME', branches, 'Branch choice')
+        }
+        scm {
+            git {
+                remote {
+                    github("MNT-Lab/d323dsl", "https")
+                }
+                branch('$BRANCH_NAME')
+            }
+        }
+        steps {
+            shell('bash script.sh > output.txt')
+            shell('tar cvf $BRANCH_NAME\\_dsl_script.tar.gz jobs.groovy')
+        }
+        publishers {
+            archiveArtifacts {
+                pattern("output.txt, *.tar.gz")
+                allowEmpty(false)
+                onlyIfSuccessful(false)
+                fingerprint(false)
+                defaultExcludes(true)
+            }
+        }
     }
 }

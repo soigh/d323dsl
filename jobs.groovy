32a459c7e7b4bdcdc4abe301b07effcf54889bb6
EugenePapkou <eugene.papkou@gmail.com>
2018-07-31 15:46:10 +0300

    change jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -4,10 +4,10 @@ freeStyleJob('MNTLAB-ypapkou-main-build-job'){
     choiceParam('BRANCH_NAME', ['ypapkou', 'master'], 'Branch name')
     activeChoiceParam('Jobs_to_execute') {
       description('Allows user choose jobs to execute')   	  
-      filterable()
+      filterable(false)
       choiceType('CHECKBOX')
       groovyScript {
-        script('return ["MNTLAB-ypapkou-child1-build-job", "MNTLAB-ypapkou-child2-build-job", "MNTLAB-ypapkou-child3-build-job", "MNTLAB-ypapkou-child4-build-job"]\n')
+        script('''return ["MNTLAB-ypapkou-child1-build-job","MNTLAB-ypapkou-child2-build-job","MNTLAB-ypapkou-child3-build-job","MNTLAB-ypapkou-child4-build-job"]''')
      	fallbackScript('"fallback choice"')
       }
     }

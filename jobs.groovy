47c8b216fa612dc1e4560114c5ae4faf98543523
Darya Zhukova <Darya_Zhukova@epam.com>
2018-07-31 09:20:19 +0100

    5 jobs added

--- jobs.groovy
+++ jobs.groovy
@@ -2,8 +2,33 @@ job('MNTLAB-dzhukova-main-build-job') {
     scm {
         git('https://github.com/MNT-Lab/d323dsl.git')
     }
-   
+
     steps {
        shell('echo "Hello"')
     }
 }
+job('MNTLAB-dzhukova-child1-build-job') {
+    
+    steps {
+       shell('echo "Hello1"')
+    }
+}
+job('MNTLAB-dzhukova-child2-build-job') {
+    
+    steps {
+       shell('echo "Hello2"')
+    }
+}
+job('MNTLAB-dzhukova-child3-build-job') {
+    
+    steps {
+       shell('echo "Hello3"')
+    }
+}
+job('MNTLAB-dzhukova-child4-build-job') {
+    
+    steps {
+       shell('echo "Hello4"')
+    }
+}
+

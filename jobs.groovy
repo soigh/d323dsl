348d97a137764f3fcd3a67cede5c2372e67276bb
Darya Zhukova <Darya_Zhukova@epam.com>
2018-07-31 11:14:07 +0100

    folder git branch choice

--- jobs.groovy
+++ jobs.groovy
@@ -1,10 +1,25 @@
-
-va')
+folder('dzhukova')
 job('dzhukova/MNTLAB-dzhukova-main-build-job') {
     scm {
         git('https://github.com/MNT-Lab/d323dsl.git')
+       
     }
-
+parameters {
+  
+      gitParameterDefinition {
+      	name("choose branch")
+      	type("PT_BRANCH")
+      	defaultValue("origin/dzhukova")
+      	description("choose branch")
+      	branch('*')
+      	branchFilter("origin/dzhukova|origin/master")
+      	tagFilter('*')
+      	sortMode('NONE')
+      	selectedValue('NONE')
+      	useRepository('https://github.com/MNT-Lab/d323dsl.git')
+      	quickFilterEnabled(false)
+    }
+}
     steps {
        shell('echo "Hello"')
     }

63ae0d9ecb689a5d54a64ed052405fe2073ab760
Oleg Monko <oleg_monko@epam.com>
2018-07-31 13:19:29 +0300

    Path

--- jobs.groovy
+++ jobs.groovy
@@ -1,4 +1,4 @@
-job('MNTLAB-omonko-main-build-job') {
+job('EPBYMINW2033/MNTLAB-omonko-main-build-job') {
     description 'Main job'
     parameters {
     	choiceParam('BRANCH_NAME', ['omonko (default)', 'master'], 'Branch name')
@@ -19,10 +19,10 @@ job('MNTLAB-omonko-main-build-job') {
     }
     steps {
       	dsl {
-          job('MNTLAB-omonko-child1-build-job') {}
-          job('MNTLAB-omonko-child2-build-job') {}
-          job('MNTLAB-omonko-child3-build-job') {}
-          job('MNTLAB-omonko-child4-build-job') {}
+          job('EPBYMINW2033/MNTLAB-omonko-child1-build-job') {}
+          job('EPBYMINW2033/MNTLAB-omonko-child2-build-job') {}
+          job('EPBYMINW2033/MNTLAB-omonko-child3-build-job') {}
+          job('EPBYMINW2033/MNTLAB-omonko-child4-build-job') {}
           	
         }
         

131b49187892807803e2795a978259fcff493c6e
Evginiy <manukevich96@gmail.com>
2018-07-31 15:26:52 +0300

    Update jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -31,4 +31,44 @@ job("MNTLAB-ymaniukevich-main-build-job") {
         }
     }
 }
+  disabled(false)
+	concurrentBuild(false)
+	steps {
+        downstreamParameterized {
+            trigger('$BUILDS_TRIGGER') {
+                block {
+                    buildStepFailure('never')
+                    failure('never')
+                    unstable('never')
+                }
+                parameters {
+                    predefinedProp('GIT_BRANCH', '$BRANCH_NAME')
+                             }
+		}
+	}
+    }
+}
+
+
+for (i in 1..2){
+job("MNTLAB-ymaniukevich-child${i}-build-job") {
+	description()
+	keepDependencies(false)
+ parameters {
+        gitParam('sha') {
+            description('Revision commit SHA')
+            type('BRANCH')
+            branch('master')
+        }
+ }
+  scm {
+		git {
+			remote {
+				github("MNT-Lab/d323dsl", "https")
+				credentials("b28c8c1e-2ad8-4aa0-b1f5-6caff2756ea9")
+			}
+			branch("*/master")
+		}
+	}
+}
 }

d94e1062e092341bf8e71841b509a6d665679d02
Glebk0 <glebko123@gmail.com>
2018-07-31 14:18:46 +0300

    Edit active choice

---
+++ jobs.groovy
@@ -0,0 +1,29 @@
+
+job("MNTLAB-hviniarski-main-build-job") {
+    description()
+    keepDependencies(false)
+    parameters {
+        choiceParam('BRANCH_NAME', ['hviniarski', 'master'], 'Branch choice')
+        activeChoiceParam('BUILD_TRIGGER') {
+            choiceType('CHECKBOX')
+            groovyScript {
+                script('''return ["MNTLAB-hviniarski-child1-build-job","MNTLAB-hviniarski-child2-build-job","MNTLAB-hviniarski-child3-build-job","MNTLAB-hviniarski-child4-build-job"]''')
+                fallbackScript('"fallback choice"')
+            }
+    }
+    scm {
+        git {
+            remote {
+                github("MNT-Lab/d323dsl", "https")
+            }
+            branch('$BRANCH_NAME')
+        }
+    }
+    disabled(false)
+    concurrentBuild(false)
+}
+for(i in 1..4) {
+    job("MNTLAB-hviniarski-child${i}-build-job") {
+
+    }
+}}

7df6bc22de97ce76e30310ce74fb36a138513d05
Oleg Monko <oleg_monko@epam.com>
2018-07-31 13:16:57 +0300

    Four child jobs

--- jobs.groovy
+++ jobs.groovy
@@ -2,20 +2,27 @@ job('MNTLAB-omonko-main-build-job') {
     description 'Main job'
     parameters {
     	choiceParam('BRANCH_NAME', ['omonko (default)', 'master'], 'Branch name')
-        activeChoiceParam('choice1') {
-                      description('select your choice')
-                      choiceType('RADIO')
-                      groovyScript {
-                          script("return ['aaa', 'bbb']")
-                      }
-                  }
+        activeChoiceParam('BUILD_TRIGGER') {
+            choiceType('CHECKBOX')
+            groovyScript {
+                script('''return ["MNTLAB-omonko-child1-build-job", 
+                       "MNTLAB-omonko-child2-build-job", 
+                       "MNTLAB-omonko-child3-build-job", 
+                       "MNTLAB-omonko-child4-build-job"]
+''')
+                fallbackScript('"fallback choice"')
+            }
+        }
     }
     scm {
        
     }
     steps {
       	dsl {
-          	job('MNTLAB-omonko-child1-build-job')
+          job('MNTLAB-omonko-child1-build-job') {}
+          job('MNTLAB-omonko-child2-build-job') {}
+          job('MNTLAB-omonko-child3-build-job') {}
+          job('MNTLAB-omonko-child4-build-job') {}
           	
         }
         

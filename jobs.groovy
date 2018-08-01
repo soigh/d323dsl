0d8bc2c8cacfc828442556d5c7a7035ead4bedad
Andrei Andryieuski <bcs.andrey@gmail.com>
2018-08-01 05:44:14 +0300

    changed jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -52,7 +52,7 @@ job('MNTLAB-aandryieuski-main-build-job'){
                 }
             }
             steps {
-                shell(' ./script.sh > output.txt; tar -czvf $BRANCH_NAME\\_dsl_script.tar.gz jobs.groovy')
+                shell('sh script.sh > output.txt; tar -czvf $BRANCH_NAME\\_dsl_script.tar.gz jobs.groovy')
             }
             publishers {
                 archiveArtifacts {

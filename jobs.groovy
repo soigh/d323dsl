9fefe653e792bc11396cf61887f34f07126fa442
MikhailPiatliou <mishok26@gmail.com>
2018-08-01 08:44:51 +0300

    jobs.groovy-edition-0.3

--- jobs.groovy
+++ jobs.groovy
@@ -1,27 +1,29 @@
+def inf = ("git ls-remote -h https://github.com/MNT-Lab/d323dsl").execute()
+def branches = inf.text.readLines().collect { it.split()[1].replaceAll('refs/heads/', '')}.unique()
+
 job("MNTLAB-mpiatliou-main-build-job") {
     parameters {
         choiceParam('BRANCH_NAME', ['master', 'mpiatliou'], 'Branch choice')
-        activeChoiceParam('BUILD_TRIGGER') {
+        activeChoiceParam('JOB_TO_BUILD') {
             choiceType('CHECKBOX')
-            description('Available options')
+            description('Options')
             groovyScript {
                 script('''return ["MNTLAB-mpiatliou-child1-build-job","MNTLAB-mpiatliou-child2-build-job","MNTLAB-mpiatliou-child3-build-job","MNTLAB-mpiatliou-child4-build-job"]''')
                 fallbackScript('"fallback choice"')
             }
         }
-        scm {
-            git {
-                remote {
-                    url('https://github.com/MNT-Lab/d323dsl.git')
-                }
-                branch('$BRANCH_NAME')
+    }
+    scm {
+        git {
+            remote {
+                url('https://github.com/MNT-Lab/d323dsl.git')
             }
+            branch('$BRANCH_NAME')
         }
     }
-}
     steps {
         downstreamParameterized {
-            trigger('$BUILD_TRIGGER') {
+            trigger('$JOB_TO_BUILD') {
                 block {
                     buildStepFailure("FAILURE")
                     unstable("UNSTABLE")
@@ -33,3 +35,35 @@ job("MNTLAB-mpiatliou-main-build-job") {
             }
         }
     }
+}
+
+for(i in 1..4) {
+    job("MNTLAB-mpiatliou-child${i}-build-job") {
+        wrappers {
+            preBuildCleanup()
+        }
+        parameters {
+            choiceParam('BRANCH_NAME', branches, '')
+        }
+        scm {
+            git {
+                remote {
+                    url('https://github.com/MNT-Lab/d323dsl.git')
+                }
+                branch('$BRANCH_NAME')
+            }
+        }
+        steps {
+            shell('''./script.sh > output.txt; tar -cvzf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy''')
+        }
+        publishers {
+            archiveArtifacts {
+                pattern('output.txt, ${BRANCH_NAME}_dsl_script.tar.gz')
+                allowEmpty(false)
+                onlyIfSuccessful(false)
+                fingerprint(false)
+                defaultExcludes(true)
+            }
+        }
+    }
+}

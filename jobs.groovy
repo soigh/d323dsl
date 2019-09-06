770a229989445c28c1fc3820ee702a1be664e221
Oksana <53932708+soigh@users.noreply.github.com>
2019-09-06 13:59:24 +0300

    Update jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -1,62 +1,27 @@
 def git_info = ("git ls-remote -h https://github.com/soigh/d323dsl.git").execute()
+// доступ к удаленному репозиторию
 def branches = git_info.text.readLines().collect { it.split()[1].replaceAll('refs/heads/', '')}.unique()
-
-for (i in 1..4) {
-  job("MNTLAB-osamorukova-child${i}-build-job") {
-    scm {
-      git {
-        remote {
-          url('https://github.com/soigh/d323dsl.git')
-        }
-        branch('$BRANCH_NAME')
-      }
-    }
-    parameters {
-      choiceParam('BRANCH_NAME', branches, '')
-    }
-    steps {
-      shell('''
-chmod +x script.sh
-./script.sh > output.txt
-tar -czf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy''')
-    }
-    publishers {
-		archiveArtifacts {
-          pattern('output.txt, ${BRANCH_NAME}_dsl_script.tar.gz')
-			allowEmpty(false)
-			onlyIfSuccessful(false)
-			fingerprint(false)
-			defaultExcludes(true)
-		}
-	}
-  }
-}
+// список бранчей
 
 job('MNTLAB-osamorukova-main-build-job') {
-    scm {
-      git {
-        remote {
-          url('https://github.com/soigh/d323dsl.git')
-        }
-        branch('$BRANCH_NAME')
-      }
-    }
-    parameters {
-        choiceParam('BRANCH_NAME', ["osamorukova", "master"], '')
-        activeChoiceParam('BUILD_TRIGGER') {
-            description('Available options')
-            choiceType('CHECKBOX')
-            groovyScript {
-              script('''return ["MNTLAB-osamorukova-child1-build-job",
+	parameters {
+		choiceParam('BRANCH_NAME', ["osamorukova", "master"], 'Branches')
+// создание параметризированной джобы, параметр BRANCH_NAME, варианты: osamorukova, master
+		activeChoiceParam('BUILD_TRIGGER') {
+			description('Available options')
+			choiceType('CHECKBOX')
+			groovyScript {
+				script('''return ["MNTLAB-osamorukova-child1-build-job",
 "MNTLAB-osamorukova-child2-build-job",
 "MNTLAB-osamorukova-child3-build-job",
 "MNTLAB-osamorukova-child4-build-job"]''')
-              fallbackScript('"fallback choice"')
-            }
-        }
+				fallbackScript('return [error]')
+// параметр BUILD_TRIGGER, мультивыбор из списка дочерних джоб
+			}
+		}
         
-    }
-    steps {
+	}
+	steps {
 		downstreamParameterized {
 			trigger('$BUILD_TRIGGER') {
 				block {
@@ -64,9 +29,35 @@ job('MNTLAB-osamorukova-main-build-job') {
 					unstable("UNSTABLE")
 					failure("FAILURE")
 				}
-                parameters {
-                    predefinedProp('BRANCH_NAME', '$BRANCH_NAME')
-                }
+				parameters {
+					predefinedProp('BRANCH_NAME', '$BRANCH_NAME')
+// запуск нового параметризированного билда
+				}
+			}
+		}
+	}
+}
+
+for (i in 1..4) { 
+// цикл для перебора элементов списка
+	job("MNTLAB-osamorukova-child${i}-build-job") {
+		parameters {
+			choiceParam('BRANCH_NAME', branches, '')
+		}
+// создание дочерних параметризированных джоб
+		steps {
+			shell('''./script.sh > output.txt
+tar -czf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy''')
+		}
+		publishers {
+			archiveArtifacts {
+				pattern('output.txt, ${BRANCH_NAME}_dsl_script.tar.gz')
+				allowEmpty(false)
+				onlyIfSuccessful(true)
+				fingerprint(false)
+				defaultExcludes(true)
+/* шаг билда - исполнение скрипта и его вывод в текстовый файл, архивирование jobs.groovy,
+также создание артифактов джобы */ 
 			}
 		}
 	}
